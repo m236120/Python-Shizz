309d1fe6c2e19277e0d2c63fc579f3466ec25233
m236120 <69885750+m236120@users.noreply.github.com>
2021-04-13 11:17:33 -0400

    Add files via upload

---
+++ CovidSurvey.py
@@ -0,0 +1,26 @@
+import time
+import random
+from pynput.keyboard import Key, Controller
+
+arr = [Key.tab,Key.tab,"13",Key.tab,"Sturdifen",Key.tab,"Dale",Key.tab,Key.down,Key.tab,Key.enter,1,Key.tab,Key.tab,Key.right,Key.tab,Key.right,Key.tab,Key.right,Key.tab,Key.right,Key.tab,Key.right,Key.tab,Key.right,Key.tab,Key.right,Key.tab,Key.right,Key.tab,Key.down,Key.tab,Key.down,Key.tab,Key.tab,Key.enter,1,Key.tab,Key.tab,Key.tab,Key.tab,];
+print("Waiting.");
+time.sleep(1);
+print("Waiting..");
+time.sleep(1);
+print("Waiting...");
+time.sleep(1);
+
+keyboard = Controller();
+
+print("Started!");
+for e in arr:
+    if isinstance(e, str):
+        keyboard.type(e);
+    elif isinstance(e, float) or isinstance(e, int):
+        time.sleep(e);
+    else:
+        keyboard.press(e);
+        keyboard.release(e);
+    delay = random.uniform(0.1, .4);
+    time.sleep(delay);
+print("Done.");
